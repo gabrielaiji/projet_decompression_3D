b2688c413b1c598765d52b6620bd5772df32135e
LAVAUR Jonas <jlavaur@salameche.enseeiht.fr>
2023-10-13 12:05:21 +0200

    Fonction convert_obj_to_list_faces finie et testée

--- src/convert_obj_to_list_faces.py
+++ src/convert_obj_to_list_faces.py
@@ -1,4 +1,17 @@
 import obja
+from objects import Vertex, Face
 
 def convert_obj_to_list_faces(obj:obja.Model):
-    
+
+    list_vertices = []
+    for (vertex_index, vertex) in enumerate(obj.vertices):
+        my_vertex = Vertex(vertex_index + 1, vertex[0], vertex[1], vertex[2]) # "+ 1" car les indices commencent à partir de 1
+        list_vertices.append(my_vertex)
+
+    list_faces = []
+    for (face_index, face) in enumerate(obj.faces):
+        list_3_vertices = [list_vertices[face.a], list_vertices[face.b], list_vertices[face.c]]
+        my_face = Face(face_index + 1, list_3_vertices) # "+ 1" car les indices commencent à partir de 1
+        list_faces.append(my_face)
+
+    return list_faces    
---
+++ src/test_conversion.py
@@ -0,0 +1,8 @@
+import obja
+from convert_obj_to_list_faces import convert_obj_to_list_faces
+
+model = obja.parse_file('../example/bunny.obj')
+list_faces = convert_obj_to_list_faces(model)
+my_list_map = list(map(lambda face : face.toString(), list_faces))
+for face in my_list_map:
+    print(face)
