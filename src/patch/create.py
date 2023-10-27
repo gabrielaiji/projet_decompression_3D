f68f6aecf4ede01ffc7f96707ab0fde220876ba1
Rochaix--yamamoto Gabriel <gabriel.rochaix@me.com>
2023-10-27 11:43:24 +0200

    orderVertices

--- src/patch/create.py
+++ src/patch/create.py
@@ -16,7 +16,7 @@ def create_z_simple(liste_vertices):
     i = 0
     j = 2
     compteur = 0
-    while compteur<nb_f_cote
+    while compteur<nb_f_cote:
         result.append((liste_vertices[i%n],liste_vertices[j],liste_vertices[(i-1)%n]))
         compteur += 1
         i -= 1
@@ -26,7 +26,7 @@ def create_z_simple(liste_vertices):
     i = 2
     j = n-1
     compteur = 0
-    while compteur<nb_f_cote
+    while compteur<nb_f_cote:
         result.append((liste_vertices[i],liste_vertices[j],liste_vertices[i+1]))
         compteur += 1
         i += 1
---
+++ src/patch/order_vertices.py
@@ -0,0 +1,44 @@
+from objects import Vertex, Face
+from coloration.dsatur import add_values_in_dict
+
+
+def orderVertices(faces: list[Face], v_delete: Vertex) -> list[Vertex]:
+    """
+    Orders the vertices in a patch to
+    make it easier to create the faces
+    of the patch.
+
+    :param faces: old faces to be deleted.
+    :param v_delete: vertex to be deleted.
+    :output: Vertices on the border of the patch
+    in the order.
+    """
+
+    related_v = {}
+    for i, face in enumerate(faces):
+        vertices = face.getVertices()
+        vertices.remove(v_delete)
+        
+        v0 = vertices[0]
+        v1 = vertices[1]
+
+        add_values_in_dict(related_v, v0, v1)
+        add_values_in_dict(related_v, v1, v0)
+
+        if i == 0:
+            v_start = v0
+
+    ordered_v = [v_start]
+    for i in range(len(faces)-1):
+        potential_next_v = related_v[ordered_v[i]]
+
+        potential_indice = 0
+        while potential_next_v[potential_indice] in ordered_v:
+            potential_indice +=1
+        
+        ordered_v.append(potential_next_v[potential_indice])
+    
+    return ordered_v
+
+
+    
