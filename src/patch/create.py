4da09f587deb7d2efc3fa4275e33afef0e6b20a0
Rochaix--yamamoto Gabriel <gabriel.rochaix@me.com>
2023-10-27 12:02:06 +0200

    create_all_patches

--- src/patch/create.py
+++ src/patch/create.py
@@ -1,13 +1,40 @@
-def create_z_simple(liste_vertices):
+from objects import Vertex, Face, Patch
+from .order_vertices import orderVertices
+
+
+
+def create_all_patches(list_vertices_to_delete: list[Vertex], id_f_start: int)\
+    -> tuple[dict[int, dict[str, list[Face]|Patch]], list[Patch]]:
+
+    patches = {}
+    lst_patches = []
+    for v_to_be_deleted in list_vertices_to_delete:
+        old_faces = v_to_be_deleted.getFaces()
+        ordered_v = orderVertices(old_faces)
+
+        new_faces_local, id_f_start = create_z_simple(ordered_v, id_f_start)
+        patch_id = v_to_be_deleted.id()
+        patch = Patch(patch_id, new_faces_local, v_to_be_deleted)
+
+        patches[patch_id] = {"old_faces": old_faces, "new_faces":new_faces_local, "patch":patch}
+        lst_patches.append(patch)
+
+
+    return patches, lst_patches
+
+
+def create_z_simple(liste_vertices: list[Vertex], id_f_start: int):
 
     result = []
 
     n = len(liste_vertices)
 
     # Ajouter le face tout en haut
-    result.append((liste_vertices[0],liste_vertices[1],liste_vertices[2]))
+    result.append(Face(id_f_start, liste_vertices[0],liste_vertices[1],liste_vertices[2]))
+    id_f_start +=1
     # Ajouter le face tout en bas
-    result.append((liste_vertices[n-1],liste_vertices[n-2],liste_vertices[n-3]))
+    result.append(Face(id_f_start,liste_vertices[n-1],liste_vertices[n-2],liste_vertices[n-3]))
+    id_f_start +=1
 
     # le nombre des faces de chaque côté
     nb_f_cote = (n-2-2)/2
@@ -17,7 +44,8 @@ def create_z_simple(liste_vertices):
     j = 2
     compteur = 0
     while compteur<nb_f_cote:
-        result.append((liste_vertices[i%n],liste_vertices[j],liste_vertices[(i-1)%n]))
+        result.append(Face(id_f_start, liste_vertices[i%n],liste_vertices[j],liste_vertices[(i-1)%n]))
+        id_f_start +=1
         compteur += 1
         i -= 1
         j += 1
@@ -27,9 +55,10 @@ def create_z_simple(liste_vertices):
     j = n-1
     compteur = 0
     while compteur<nb_f_cote:
-        result.append((liste_vertices[i],liste_vertices[j],liste_vertices[i+1]))
+        result.append(Face(id_f_start, liste_vertices[i],liste_vertices[j],liste_vertices[i+1]))
+        id_f_start +=1
         compteur += 1
         i += 1
         j -= 1
 
-    return result
+    return result, id_f_start
